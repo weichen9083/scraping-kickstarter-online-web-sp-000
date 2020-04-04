bae0f8db40b144ac623dc40917bf2f265e190d03
wei chen <weichen9083@gmail.com>
2020-04-04 19:39:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,17 +6,3 @@ require 'pry'
 
 
 
-
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry 
-end
-
-create_project_hash 
-
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
